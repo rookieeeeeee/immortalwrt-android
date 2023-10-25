2099f16e371fd0e3d00b6455165463bcda5d4a35
rrkk <2890023401@qq.com>
2023-10-25 18:37:38 +0800

    Add files via upload

--- package/kernel/linux/modules/android.mk
+++ package/kernel/linux/modules/android.mk
@@ -1,17 +1,25 @@
+include $(TOPDIR)/rules.mk
+include $(INCLUDE_DIR)/kernel.mk
+
 ANDROID_MENU:=Android modules
 
+
 define KernelPackage/android
-	SUBMENU:=$(ANDROID_MENU)
-	TITLE:=Android Support
-	KCONFIG:=ANDROID
-	FILES:= \
-		$(LINUX_DIR)/drivers/android/binderfs.ko \
-		$(LINUX_DIR)/drivers/staging/android/ashmem.ko
-	AUTOLOADD:=$(call AutoProbe,binderfs,ashmem)
+  SUBMENU:=$(ANDROID_MENU)
+  TITLE:=Android Support
+  KCONFIG:=CONFIG_ANDROID \
+	   CONFIG_ANDROID_BINDERFS=y \
+	   CONFIG_ANDROID_BINDER_IPC=y \
+	   CONFIG_ASHMEM=y
+  FILES:= \
+    $(LINUX_DIR)/drivers/android/binderfs.ko \
+    $(LINUX_DIR)/drivers/staging/android/ashmem.ko
+  AUTOLOAD:=$(call AutoLoad,30,binderfs,ashmem)
 endef
 
 define KernelPackage/android/description
-	Kernel module for android(binderfs,ashmem)
+ Kernel module for android(binderfs,ashmem)
 endef
+
 
 $(eval $(call KernelPackage,android))
